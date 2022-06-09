3a69479c4f9c7b31cc445ab7123e772d86d89775
Cweili <3weili@gmail.com>
2022-06-09 16:49:47 +0800

    feat: support resolve relative path

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # VM Worker
 
 [![npm][badge-version]][npm]
-<!-- [![bundle size][badge-size]][bundlephobia] -->
 [![npm downloads][badge-downloads]][npm]
 [![license][badge-license]][license]
 [![github][badge-issues]][github]
-<!-- [![build][badge-build]][workflows]
-[![coverage][badge-coverage]][coveralls] -->
 
 Tiny VM for browser to execute commonjs modules in Web Worker.
 
@@ -17,20 +14,24 @@ App.js
 ```js
 import VM from 'vm-worker'
 
-const vm = VM()
+const vm = VM({
+  timeout: 100000 // default 100000ms
+})
 
 await vm.require([
   {
-    path: 'a.js',
+    path: '/dirA/a.js',
     url: 'https://xxx.com/a.js',
   },
   {
-    path: 'b.js',
-    src: 'module.exports = require("./a")',
+    path: '/dirB/b.js',
+    src: 'module.exports = require("../dirA/a")',
   },
 ])
 
 await vm.exec('b.js', 1, 2) // => 3
+
+vm.terminate()
 ```
 
 a.js
@@ -51,8 +52,8 @@ npm i vm-worker
 [badge-downloads]: https://img.shields.io/npm/dt/vm-worker.svg
 [npm]: https://www.npmjs.com/package/vm-worker
 
-<!-- [badge-size]: https://img.shields.io/bundlephobia/minzip/vm-worker.svg
-[bundlephobia]: https://bundlephobia.com/result?p=vm-worker -->
+[badge-size]: https://img.shields.io/bundlephobia/minzip/vm-worker.svg
+[bundlephobia]: https://bundlephobia.com/result?p=vm-worker
 
 [badge-license]: https://img.shields.io/npm/l/vm-worker.svg
 [license]: https://github.com/Cweili/vm-worker/blob/master/LICENSE
@@ -60,8 +61,8 @@ npm i vm-worker
 [badge-issues]: https://img.shields.io/github/issues/Cweili/vm-worker.svg
 [github]: https://github.com/Cweili/vm-worker
 
-<!-- [badge-build]: https://img.shields.io/github/workflow/status/Cweili/vm-worker/ci/master
+[badge-build]: https://img.shields.io/github/workflow/status/Cweili/vm-worker/ci/master
 [workflows]: https://github.com/Cweili/vm-worker/actions/workflows/ci.yml?query=branch%3Amaster
 
 [badge-coverage]: https://img.shields.io/coveralls/github/Cweili/vm-worker/master.svg
-[coveralls]: https://coveralls.io/github/Cweili/vm-worker?branch=master -->
+[coveralls]: https://coveralls.io/github/Cweili/vm-worker?branch=master
--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-new-func, no-restricted-globals */
+import resolvePath from 'resolve-pathname'
+
+const requireCache = new Map()
 
 async function fetchScript(url) {
   const resp = await fetch(url, {
@@ -7,28 +10,44 @@ async function fetchScript(url) {
   return resp.text()
 }
 
-const requireCache = new Map()
-
-function require(path) {
-  const module = {
-    exports: {},
-  }
-
-  const filenameWithoutExt = path.replace(/^\.\//, '').replace(/\.js$/, '')
-  const filename = `${filenameWithoutExt}.js`
-  const dirname = '.'
-  const fn = requireCache.get(filename) || requireCache.get(filenameWithoutExt)
-  if (fn) {
-    fn(module.exports, require, module, filename, dirname)
-    return module.exports
-  }
-  throw new Error(`module "${path}" not found`)
+function getAbsolutePath(path) {
+  return `${path[0] === '/' ? '' : '/'}${path}`
 }
+
+function getRequire(baseDir = '/') {
+  return (path) => {
+    const module = {
+      exports: {},
+    }
+
+    const filenameWithoutExt = path.replace(/\.js$/, '').replace(/^\S*?([^/]+)$/, '$1')
+    const filename = `${filenameWithoutExt}.js`
+    const dirname = resolvePath(path.replace(/^(\S*?)[^/]+$/, '$1'), baseDir).replace(/\/$/, '')
+    const dirnameWithSlash = `${dirname}/`
+    const isRoot = !dirname.length
+    const fn = requireCache.get(dirnameWithSlash + filename)
+      || requireCache.get(dirnameWithSlash + filenameWithoutExt)
+      || requireCache.get(`${dirnameWithSlash}${filenameWithoutExt}/index.js`)
+    if (fn) {
+      fn(
+        module.exports,
+        getRequire(dirnameWithSlash),
+        module,
+        filename,
+        isRoot ? '/' : dirname,
+      )
+      return module.exports
+    }
+    throw new Error(`module "${path}" not found`)
+  }
+}
+
+const requireFn = getRequire()
 
 const fns = {
   async require(files) {
     const scripts = (await Promise.all(files.map(async (file) => ({
-      path: file.path,
+      path: getAbsolutePath(file.path),
       fn: new Function(
         'exports',
         'require',
@@ -44,8 +63,7 @@ const fns = {
   },
 
   async exec(path, ...args) {
-    // eslint-disable-next-line import/no-dynamic-require
-    return require(path)(...args)
+    return requireFn(path)(...args)
   },
 }
 
